a1f70f172d6330abdca2ab9de91656690c630bfa
root <root@Ty-Laptop.localdomain>
2022-11-03 11:25:43 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 8;
+let num2 = 8;
+let multiply = num1 * num2 - 2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 5) + 1;
+
+
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4;
+
+
+
+ let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
